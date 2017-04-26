2b8825df39555c3841c7f0034b69568b0409600d
tu-laptop <tugrulerturk@windowslive.com>
2017-04-26 16:52:35 +0200

    Initial Commit

---
+++ AnaphoraCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AnaphoraCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://localhost:5100", "http://localhost:5101", "http://*:5102")
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
