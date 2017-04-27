dc971971ca22af66533d150bbbb5562cac1db883
tu-laptop <tugrulerturk@windowslive.com>
2017-04-27 19:55:46 +0200

    -Microsoft Cognitive Services Linguistics API implemented.

--- AnaphoraCore/Controllers/HomeController.cs
+++ AnaphoraCore/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace AnaphoraCore.Controllers
 {
@@ -29,6 +25,12 @@ namespace AnaphoraCore.Controllers
 
         public IActionResult Error()
         {
+            return View();
+        }
+
+        public IActionResult ResolveAnaphora(string sentence)
+        {
+
             return View();
         }
     }
